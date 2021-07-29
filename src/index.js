c0659a3804452f079d1e76d75266bb9eb9871571
MAC <mac@DREAMSs-iMac-Pro.local>
2021-07-29 17:17:33 +0530

    finallogin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 require('file-loader?name=[name].[ext]!./index.html');
 import React from 'react';
+import {App} from './App';
+import './App.scss';
+import {Provider} from 'react-redux';
 import ReactDom from 'react-dom';
-import { App } from './App';
-import './App.scss'
+import { store } from './helpers';
+import { configureFakeBackend } from './helpers';
 
-const appElement  = document.getElementById('app');
-
-ReactDom.render (<App />, appElement);
+configureFakeBackend();
+ReactDom.render (
+     <Provider store={store}>
+     <App />
+     </Provider>,
+      document.getElementById('app')
+      );
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,44 @@
 const path = require('path');
-const MiniCssExtraPlugin = require('mini-css-extract-plugin');
+const loader = require('sass-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    output:{
-        path : path.join(__dirname, '/dist'),
-        filename :   'index.bundle.js',
-    },
-    devServer:{
-        port: 3010,
-        watchContentBase : true,
-    },
-    module:{
-        rules:[
-            {
-                test:/\.(js|jsx)$/,
-                exclude: /node_modules/,    
-                use:{
-                    loader:'babel-loader'
-                }
-            },
-            {
-                test:/\.scss$/,
-                use:[
-                    MiniCssExtraPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ],
-            }
-        ]
-    },
-    plugins:[new MiniCssExtraPlugin()],
+        mode: 'development',
+        output:{
+            path: path.join(__dirname, '/dist'),
+            filename: 'index.bundle.js',
+        },
+        devServer:{
+            port: 3020,
+            historyApiFallback: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                            'css-loader',
+                            'sass-loader'
+                    ],
+                },
+            ]
+        },
+        externals: {
+            // global app config object
+            config: JSON.stringify({
+                apiUrl: 'http://localhost:3020'
+            })
+        },
+        plugins: [new MiniCssExtractPlugin()],
+        // plugins: [new HtmlWebpackPlugin({
+        //     template: './src/index.html'
+        // })],
 };
