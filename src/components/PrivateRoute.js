6b8c927713e48f3966eba89905918c82dc7a662b
MAC <mac@DREAMSs-iMac-Pro.local>
2021-07-30 18:47:41 +0530

    templateintrigation

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router,Route, Redirect } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Router>
-        <Switch>
     <Route {...rest} render={props => (
         localStorage.getItem('user')
             ? <Component {...props} />
             : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-    )} /></Switch></Router>
+    )} />
 )
