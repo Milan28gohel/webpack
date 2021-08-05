aa97489f192ffd704554668115215bed843ba50c
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-05 12:37:43 +0530

    contactadd

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
 import  {RegisterPage } from './RegisterPage';
 import {Footer} from './Footer';
+import Contact from './Contact/Contact';
 
 
 
@@ -38,6 +39,7 @@ class App extends React.Component{
                                 <PrivateRoute exact path="/" component={HomePage} />
                                 <Route path="/login" component={LoginPage} />
                                 <Route path="/register" component={RegisterPage} />
+                                <Route path="/contact" component={Contact} />
                                 <Redirect from="*" to="/" />
                             </Switch>
                         </Router>
