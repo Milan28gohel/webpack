6dc5ca071fb7ccb555883ff6f07bb4d1e758a19a
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-05 13:01:38 +0530

    contactadd

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
+
 app.set('port', process.env.PORT || 8080);
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+devServer.use(historyApiFallback({
+    // Paths with dots should still use the history fallback.
+    // See https://github.com/facebookincubator/create-react-app/issues/387.
+    disableDotRule: true
+  }));
 
 var server = app.listen(app.get('port'), function() {
   console.log('http://localhost:',server.address().port);
