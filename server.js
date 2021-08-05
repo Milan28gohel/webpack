d42e63126e6135ed0c9b416ae46373f2ed2346ea
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-05 14:00:00 +0530

    ok

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ app.set('port', process.env.PORT || 8080);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var app = connect()
-  .use(history())
-  .listen(8080);
+var express = require('express');
+ 
+var app = express();
+app.use(history());
 
 
 var server = app.listen(app.get('port'), function() {
